ce19cffb815ebb0a19b14b34373e8f28b730f1ff
Nhu Doan <nhu_dth235718@student.agu.edu.vn>
2025-08-28 23:10:09 +0700

    Nop bai tap Chuong 1

---
+++ baitap_vd/baitap_vd.py
@@ -0,0 +1,38 @@
+import math
+import os
+import sys
+
+import module1
+import module2
+
+import math_libs
+print(math_libs.add(4,5))
+
+print('Hello World!')
+print('Albert Einstein đã từng nói: Một người không bao giờ mắt sai lầm không boa giờ thử bất cứ điều gì mới.')
+
+if 5 > 2:
+    print('Five is greater than two!')
+
+total = 1 + \
+2 +\
+3
+print(total); print('Hello Python!')
+
+if True:
+    print('True')
+else:
+    print('False')
+
+#This is comment
+print('Hello, World!')
+
+print("Tham so 1:", sys.argv[1])
+print("Tham so 2:", sys.argv[2])
+print("Tham so 3:", sys.argv[3])
+# python baitap_vd.py 1 2 3
+
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
